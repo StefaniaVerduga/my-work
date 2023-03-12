e8129a7903cfc4e6cdbeea85a9f7c55914a43213
StefaniaVerduga <g00425680@atu.ie>
2023-03-12 22:39:40 +0000

    week 04

---
+++ week04/lab4.2.2guess1.py
@@ -0,0 +1,13 @@
+# lab4.2.2guess1.py
+# The program prompts the user to guess a number,
+# the program should keep prompting the user to guess
+# the number until the user gets the right on.
+
+numberToGuess = 30
+
+guess = int(input("Please, guess the number: "))
+while guess != numberToGuess:
+    print("Wrong")
+    guess = int(input("Please, guess again: "))
+
+print("Well done! Yes, the number was ", numberToGuess)
