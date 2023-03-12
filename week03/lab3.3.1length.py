067643b3e1633412c927aeea1665e709264a7aef
StefaniaVerduga <g00425680@atu.ie>
2023-03-12 15:53:59 +0000

    week 03

---
+++ week03/lab3.3.1length.py
@@ -0,0 +1,7 @@
+# lab3.3.1length.py
+# This program reads in a String and outputs its length
+# Author: Stefania Verduga
+
+inputString = input('Please, enter a string: ')
+lengthOfString = len(inputString)
+print(f'The length of {inputString} is {lengthOfString}')
