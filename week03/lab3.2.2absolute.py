137ef6855923a70bdf967fca0c1dbccfa3171d3d
StefaniaVerduga <g00425680@atu.ie>
2023-03-12 11:52:50 +0000

    week 03

---
+++ week03/lab3.2.2absolute.py
@@ -0,0 +1,9 @@
+# lab3.2.2absolute.py
+# This program gives the absolute value of a number
+# Author: Stefania Verduga
+
+# We should be dealing with floats 
+# As we will give the absolute value
+number = float(input("Enter a number: "))
+absoluteValue = abs(number)
+print('The absolute value of {} is {}'.format(number, absoluteValue))
