7e59cc8370e3c98cb0d323d14efd7e47e12d3377
StefaniaVerduga <g00425680@atu.ie>
2023-03-12 14:42:51 +0000

    week 03

---
+++ week03/lab3.2.3floor.py
@@ -0,0 +1,9 @@
+# lab3.2.3floor.py
+# This program floors a number
+# Author: Stefania Verduga
+
+import math
+
+numberTofloor = float(input("Enter a float number: "))
+flooredNumber = math.floor(numberTofloor)
+print('{} floored is {}'.format(numberTofloor, flooredNumber))
